88b694b1d8d12aa146ca15341849d4acd951eee7
Waris Bakare <bakarewariz012@gmail.com>
2024-01-05 22:07:03 +0100

    Initial Commit

---
+++ MobiliseTask/MobiliseTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobiliseTaskApp.swift
+//  MobiliseTask
+//
+//  Created by Bakare Waris on 05/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MobiliseTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
